84aa6da3f1239d4afb3a9ceac662f089b9591728
Vitor-Klein <vitorhklein2@gmail.com>
2025-05-27 17:00:29 -0300

    ....

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import "./style.css";
+
+function Home() {
+  return (
+    <>
+      <div>
+        <h1>Hello world</h1>
+      </div>
+    </>
+  );
+}
+
+export default Home;
